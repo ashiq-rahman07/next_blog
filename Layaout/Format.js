9a2cde0b7780c296838cf52c8b3a492675696e93
Ashiqur Rahman <web3.0.ashiq@gmail.com>
2023-01-24 11:13:51 +0600

    header section add

---
+++ Layaout/Format.js
@@ -0,0 +1,14 @@
+import Footer from "../components/footer"
+import { Header } from "../components/Header"
+
+const Format = () => {
+  return (
+   <>
+   <Header></Header>
+   <main>main content</main>
+   <Footer></Footer>
+   </>
+  )
+}
+
+export default Format
